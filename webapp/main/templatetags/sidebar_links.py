146b2c4691696c9450afb3976eedec8ab3c4a33f
damianjuszczak <244980@student.uek.krakow.pl>
2026-03-20 09:37:19 +0100

    Add files via upload
    ver 1.0

---
+++ webapp/main/templatetags/sidebar_links.py
@@ -0,0 +1,31 @@
+from django import template
+
+register = template.Library();
+
+@register.simple_tag
+def get_links():
+    return [
+        
+    #     {
+    #     'name': 'Home',
+    #     'href': '/',
+    #     'icon': 'fa-house',
+    # },
+     
+      {
+        'name': 'Categories',
+        'href': '/contact',
+        'icon': 'fa-layer-group',
+    },{
+        'name': 'Sale',
+        'href': '/contact',
+        'icon': 'fa-percent',
+    }, {
+        'name': 'Contact',
+        'href': '/contact',
+        'icon': 'fa-envelope',
+    }, {
+        'name': 'Cart',
+        'href': '/about',
+        'icon': 'fas fa-shopping-basket',
+    }]
---
+++ webapp/main/views.py
@@ -0,0 +1,55 @@
+# Plik do definiowania widoków, które są renderowane za pomocą szablonizatora Jinja oraz wyświetlane w przeglądarce
+
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages #to show message back for errors
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+# Using the Django authentication system (Django Documentation)
+# https://docs.djangoproject.com/en/5.1/topics/auth/default/
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+     
+    if request.method == 'POST':
+         user = authenticate(username=request.POST['username'], password=request.POST['password'])
+         if user is not None:
+             login(request, user)
+             if request.session.get('next'):
+                return redirect(request.session.pop('next'))
+             
+             return redirect('home')
+         else:
+             messages.error(request, 'Invalid credentials')
+             return redirect('login_user')
+         
+    if request.GET.get('next'):
+        request.session['next'] = request.GET['next']
+
+    return render(request, 'main/users/login.html')
+
+def register(request):
+    if request.user.is_authenticated:
+         return redirect('home')
+    
+    if request.method == 'POST':
+        user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
+        login(request, user)
+        return redirect('home')
+    
+    return render(request, 'main/users/register.html')
+
+def logout_user(request):
+    logout(request)
+     
+    return redirect('home')
