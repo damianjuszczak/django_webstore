da8fac5226f0d348a921c697a6ee36757dca44fa
damianjuszczak <244980@student.uek.krakow.pl>
2026-04-11 12:59:19 +0200

    Feature/cart quantity
    * added cart-quantity buttons
    
    * removed __pycache__ files and refresh .gitignore

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('cart/', views.cart, name="cart"),
     path('cart/add/<int:product_id>/', views.cart_add, name='cart_add'),
     path('cart/remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
+    path('cart/decrement/<int:product_id>/', views.cart_decrement, name='cart_decrement'),
 ]
