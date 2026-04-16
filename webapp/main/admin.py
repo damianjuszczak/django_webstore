0e04b4b40bbff6a208dd3dbd828dfa175bf1a313
Karol Kurowski <245006@student.uek.krakow.pl>
2026-04-16 02:50:45 +0200

    przywrócenie oryginanej wersji admin.py

--- webapp/main/admin.py
+++ webapp/main/admin.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
 from .models import Category, Manufacturer, Product, ProductImage
 
-# To sprawi, że zdjęcia będą widoczne wewnątrz edycji produktu
-class ProductImageInline(admin.TabularInline):
-    model = ProductImage
-    extra = 1  # Ile pustych pól na zdjęcia ma się wyświetlać domyślnie
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'manufacturer', 'price', 'stock', 'is_available']
-    prepopulated_fields = {'slug': ('name',)} # Automatycznie wpisze slug, gdy będziesz pisać nazwę!
-    inlines = [ProductImageInline]
-
 admin.site.register(Category)
 admin.site.register(Manufacturer)
+admin.site.register(Product)
+admin.site.register(ProductImage)
