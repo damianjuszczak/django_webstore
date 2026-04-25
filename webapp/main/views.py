2ece4ad952a47a8615219790c2b4ab54a91e1870
damianjuszczak <244980@student.uek.krakow.pl>
2026-04-25 17:56:17 +0200

    main page max 4 random products

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -12,7 +12,7 @@ from django.db.models import Q
 
 
 def index(request):
-    products = Product.objects.filter(is_available=True)
+    products = Product.objects.filter(is_available=True).order_by('?')[:4]
     
     return render(request, 'main/index.html', {'products': products})
 
