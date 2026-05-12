68e078a1fea60d0f05a11f1425ad620350370107
damianjuszczak <244980@student.uek.krakow.pl>
2026-05-12 21:30:20 +0200

    secret_key moved to .env fille

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jnscosjmvqy4^59lzts0s_yct#b8@9_ex)%08%6v_%ek#hm3ry'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True #show errors during program usage
