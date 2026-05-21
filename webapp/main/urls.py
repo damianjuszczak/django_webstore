44a1cdf264f01e8e08a354f3daa0e20a6f99674a
damianjuszczak <244980@student.uek.krakow.pl>
2026-05-21 22:09:00 +0200

    urls updated for checkout process

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('cart/add/<int:product_id>/', views.cart_add, name='cart_add'),
     path('cart/remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
     path('cart/decrement/<int:product_id>/', views.cart_decrement, name='cart_decrement'),
+    path('checkout/', views.checkout, name='checkout'),
 
     # Checkout URLs
     path('api/order/create', views.order_create, name='order_create'),
